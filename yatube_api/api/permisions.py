b088c40e4135cd63ed9a90fe3ab78c72e855e1c4
v.bykov <bykovvlados@vk.com>
2026-04-01 13:17:01 +0400

    permissions

--- yatube_api/api/permisions.py
+++ yatube_api/api/permisions.py
@@ -1,19 +1,10 @@
 from rest_framework import permissions
-from posts.models import Post, Comment
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    """
-    Разрешает изменение только автору объекта.
-    Для остальных — только чтение.
-    """
-
     def has_object_permission(self, request, view, obj):
-        # GET, HEAD, OPTIONS всегда разрешены
         if request.method in permissions.SAFE_METHODS:
             return True
-        if isinstance(obj, Post):
-            return obj.author == request.user
-        elif isinstance(obj, Comment):
+        if hasattr(obj, 'author'):
             return obj.author == request.user
         return False
