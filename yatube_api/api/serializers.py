ab95d78556c3c8c4dcb14a23f26e4943415921b7
v.bykov <bykovvlados@vk.com>
2026-04-03 12:17:38 +0400

    d

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -8,7 +8,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username')
+        fields = ('username',)
 
 
 class GroupSerializer(serializers.ModelSerializer):
